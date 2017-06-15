6dc046b2b22397d5f6851c7d16f1ff3232ed4df2
Michael Sparks <sparksmb@gmail.com>
2017-06-15 08:10:30 -0500

    wip refactoring candidates data structure (simplify)

--- lib/sudoku_tools/grid.rb
+++ lib/sudoku_tools/grid.rb
@@ -1,26 +1,11 @@
 class SudokuTools::Grid
   def initialize(serialized_grid)
     @grid = serialized_grid
-    @rows = parse_rows
-    @columns = parse_columns
-    @boxes = parse_boxes
     initialize_candidates
   end
 
   def serialized
     @grid
-  end
-
-  def rows
-    @rows
-  end
-
-  def columns
-    @columns
-  end
-
-  def boxes
-    @boxes
   end
 
   def candidates
@@ -36,17 +21,12 @@ class SudokuTools::Grid
     row * 9 + column
   end
 
-  private
-
-  def initialize_candidates
-    path = 'spec/fixtures/candidates.json'
-    ap @candidates = JSON.parse(File.open(path).read)
-    prune_candidates_where_digit
-    prune_candidates_from_rows
-    prune_candidates_from_columns
-  end
-
   def prune_candidates_from_rows
+    @grid.split("").each_with_index do |char, index|
+      if char != "0"
+        coordinate = coordinate_to_index(index)
+      end
+    end
   end
 
   def prune_candidates_from_columns
@@ -54,50 +34,23 @@ class SudokuTools::Grid
 
   def prune_candidates_where_digit
     @grid.split("").each_with_index do |char, index|
-      if char != "0"
-        coordinate = index_to_coordinate(index)
-        @candidates[coordinate[:row]][coordinate[:column]] = ""
-      end
+      @candidates[index] = "" if char != "0"
     end
   end
 
-  def parse_rows
-    rows = []
-    (0..8).each do |row|
-      index = (9 * row)
-      rows << @grid[index..index+8]
+  private
+
+  def scan_grid
+    @grid.split("").each_with_index do |char, index|
+      yield
     end
-    rows
   end
 
-  def parse_columns
-    columns = ["","","","","","","","",""]
-    (0..8).each do |row|
-      index = (9 * row)
-      (0..8).each do |column|
-        columns[column] += @grid[index + column]
-      end
-    end
-
-    columns
-  end
-
-  def parse_boxes
-    boxes = []
-    box_a = ""; box_b = ""; box_c = ""
-    (0..8).each do |row|
-      index = (9 * row)
-      box_a += @grid[index..index+2]
-      box_b += @grid[index+3..index+5]
-      box_c += @grid[index+6..index+8]
-      if row == 2 or row == 5
-        boxes << box_a; boxes << box_b; boxes << box_c
-        box_a = ""; box_b = ""; box_c = ""
-      end
-    end
-    boxes << box_a; boxes << box_b; boxes << box_c
-    box_a = ""; box_b = ""; box_c = ""
-
-    boxes
+  def initialize_candidates
+    path = 'spec/fixtures/candidates.json'
+    @candidates = JSON.parse(File.open(path).read)
+    prune_candidates_where_digit
+    prune_candidates_from_rows
+    prune_candidates_from_columns
   end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,45 +1,6 @@
 require 'spec_helper'
 
 describe SudokuTools::Grid do
-  it 'has rows' do
-    rows = ["090000307",
-            "102007569",
-            "030089214",
-            "501723006",
-            "368491752",
-            "200856103",
-            "427160030",
-            "956200401",
-            "803000020"]
-    expect(SudokuTools::Grid.new(easy_grid).rows).to eq(rows)
-  end
-
-  it 'has columns' do
-    columns= ["010532498",
-              "903060250",
-              "020180763",
-              "000748120",
-              "008295600",
-              "079316000",
-              "352071040",
-              "061050302",
-              "794623010"]
-    expect(SudokuTools::Grid.new(easy_grid).columns).to eq(columns)
-  end
-
-  it 'has boxes' do
-    boxes = ["090102030",
-             "000007089",
-             "307569214",
-             "501368200",
-             "723491856",
-             "006752103",
-             "427956803",
-             "160200000",
-             "030401020"]
-    expect(SudokuTools::Grid.new(easy_grid).boxes).to eq(boxes)
-  end
-
   it 'converts grid index to grid coordinate' do
     g = SudokuTools::Grid.new(easy_grid)
     expect(g.index_to_coordinate(0)).to eq ({ row: 0, column: 0 })
@@ -49,16 +10,18 @@ describe SudokuTools::Grid do
     expect(g.index_to_coordinate(80)).to eq ({ row: 8, column: 8 })
   end
 
-  it 'has candidates' do
-    g = SudokuTools::Grid.new(easy_grid)
-    ap g.serialized
-    ap candidates = g.candidates
-    expect(candidates).to eq easy_grid_candidates
-  end
-
-  it 'can translate a row and column to grid index' do
+  it 'can convert a grid coordinate to index' do
     g = SudokuTools::Grid.new(easy_grid)
     expect(g.coordinate_to_index(0,0)).to eq 0
     expect(g.coordinate_to_index(8,8)).to eq 80
+  end
+
+  it 'has pruned candidates' do
+    g = SudokuTools::Grid.new(easy_grid)
+    #ap g.serialized
+    g.prune_candidates_where_digit
+    #ap g.candidates
+    expect(g.candidates).to eq easy_grid_candidates_pruned_by_digit
+    expect(g.candidates).to eq easy_grid_candidates
   end
 end
